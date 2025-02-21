4d7ea78397a607d2760af5aba9a815b4d85117fa
DavidTjia <davidtjia18@gmail.com>
2025-02-21 09:05:28 +0800

    ubah path

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,27 +1,17 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
-// import About from "./pages/About";
 import Layout from "./Layout";
 import Winner from "./pages/Winner";
 import WhatsNew from "./pages/WhatsNew";
 import Sponsor from "./pages/Sponsor";
-// import Winner from "./pages/Winner";
-// import Guidelines from "./pages/Guidelines";
-// import Competition from "./pages/Competition";
-// import NotFound from "./pages/NotFound"; // Optional, jika ingin handle 404
 
 const routerList = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Navigate to="home" replace /> },
-      { path: "home", element: <Home /> },
+      { index: true, element: <Home /> }, // Home langsung muncul di "/"
       { path: "winner", element: <Winner /> },
       { path: "whatsnew", element: <WhatsNew /> },
       { path: "sponsor", element: <Sponsor /> },
